cf55e9ea87179af7d810db3dc5e1bf1519682d8f
Akinlua Bolamigbe Jomi <bolamigbeakinlua@gmail.com>
2021-10-16 14:38:16 +0100

    added time zone to the date

--- src/jobs.js
+++ src/jobs.js
@@ -3,6 +3,7 @@ import { sendNotification } from './modules/notify/send-mail'
 import eventService from './services/event'
 import makeMockShop from './data-access';
 import moment from 'moment';
+import moment from 'moment-timezone';
 const EventDB = makeMockShop({ modelName: 'Event' });
 const eventAttendanceDb = makeMockShop({ modelName: 'EventAttendance' });
 const { getActiveEvent } = eventService
@@ -38,7 +39,7 @@ agenda.define("Reminder1", async() => {
         }
         const evt = eventAttendance.metaDate.event
         try {
-            sendNotification({ event: "EVENT_REMINDER", data: { email: eventAttendance.email, name: eventAttendance.firstName, event: { id: event.id, email2: event.email, event_name: event.name, description: event.description, category: event.category, paid: event.paid, venue: event.venue, date: event.date.toLocaleString(), ticket_name: eventAttendance.ticket_type || 'Free', ticket_price:  `N${eventAttendance.metaDate.price || 'Free'}` , ticket_count: evt.ticket_count}} })
+            sendNotification({ event: "EVENT_REMINDER", data: { email: eventAttendance.email, name: eventAttendance.firstName, event: { id: event.id, email2: event.email, event_name: event.name, description: event.description, category: event.category, paid: event.paid, venue: event.venue, date: moment(event.date).tz('Africa/Lagos').toDate().toDateString(), ticket_name: eventAttendance.ticket_type || 'Free', ticket_price:  `N${eventAttendance.metaDate.price || 'Free'}` , ticket_count: evt.ticket_count}} })
         } catch (error) {
             console.log(error)
         }
